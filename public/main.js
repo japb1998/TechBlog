fab2a7e4cd18e49e0991e6450151a475e41b7541
Javier <javierperez@javiers-mbp.home>
2021-01-01 20:19:13 -0500

    delete button added

--- public/main.js
+++ public/main.js
@@ -1,6 +1,9 @@
+
+
 const logOutBtn = $('#logout');
 const btnComment = $('.btnComment');
-const displayCommentBtn = $('.displayComment')
+const displayCommentBtn = $('.displayComment');
+const deleteBtn = $('.delete-btn');
 function logOut(){
     
     $.post('/api/users/logout').then(()=>{
@@ -24,7 +27,17 @@ $.post('/api/blog/comment',
 
 function displayComment(){
     document.location.href = `/comment/${$(this).attr('data-comment')}`
+};
+
+function deletePost(){
+    $.ajax({
+       url: `/api/blog/${$(this).attr('data-id')}`,
+       method: 'DELETE'
+    }).then(()=>{
+        document.location.href = '/dashboard'
+    })
 }
 logOutBtn.on('click',logOut);
 btnComment.on('click',createComment);
 displayCommentBtn.on('click',displayComment);
+deleteBtn.on('click',deletePost);
